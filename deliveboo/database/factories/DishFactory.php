7c68f055042e7147236a601ceda364129858143a
Carmelo <carnamuciocarmelo@gmail.com>
2021-03-02 16:23:44 +0100

    food

--- deliveboo/database/factories/DishFactory.php
+++ deliveboo/database/factories/DishFactory.php
@@ -5,13 +5,13 @@
 use App\Dish;
 use Faker\Generator as Faker;
 
-//$faker = \Faker\Factory::create();
-//$faker->addProvider(new \FakerRestaurant\Provider\it_IT\Restaurant($faker));
+
 
 $factory->define(Dish::class, function (Faker $faker) {
+  $foodfaker = \Faker\Factory::create();
+  $foodfaker->addProvider(new \FakerRestaurant\Provider\it_IT\Restaurant($foodfaker));
     return [
-        //'name'         => $faker -> foodName(),
-        'name'         => $faker -> word,
+        'name'         => $foodfaker -> foodName(),
         'description'  => $faker -> sentence,
         'price'        =>           rand(10,100),
         'availability' =>           rand(0, 1),
