f4f2a07965ee9f3846d3dcbd330120ea005731e0
Andrea <grassiandrea95@gmail.com>
2021-03-02 17:42:21 +0100

    restaurant controller

--- deliveboo/app/Http/Controllers/RestaurantController.php
+++ deliveboo/app/Http/Controllers/RestaurantController.php
@@ -1,95 +1,50 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Restaurant;
-
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
-
 use App\User;
 use Illuminate\Http\Request;
-
 class RestaurantController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-
     public function index()
-    {  
+    {
         $userid=Auth::user()->id;
         $restaurant= User::find($userid)->restaurant;
-
         return view('home',compact('restaurant'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('restaurant-create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $data=$request->all();
+        $newRestaurant=Restaurant::create($data);
+        return redirect()->route('home');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Restaurant  $restaurant
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Restaurant $restaurant)
+    public function edit($id)
     {
-        //
+        $restaurant=Restaurant::findOrFail($id);
+        return view('restaurant-edit',compact('restaurant'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Restaurant  $restaurant
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Restaurant $restaurant)
+    public function update(Request $request, $id)
     {
-        //
+        $data=$request->all();
+        $restaurant=Restaurant::findOrFail($id);
+        $restaurant->update($data);
+        return redirect('home');
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Restaurant  $restaurant
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Restaurant $restaurant)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Restaurant  $restaurant
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Restaurant $restaurant)
-    {
-        //
+        $restaurant=Restaurant::findOrFail($id);
+        $restaurant->delete();
+        return redirect()->route('home');
     }
 }
