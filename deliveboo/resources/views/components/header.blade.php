0279fa9c2aa2825bfe5cf73f573ec53f00a629bf
Veronika <veronikakuchtova95@gmail.com>
2021-03-04 12:59:04 +0100

    Dashboard

--- deliveboo/resources/views/components/header.blade.php
+++ deliveboo/resources/views/components/header.blade.php
@@ -1,29 +1,36 @@
 <header>
-    <div>
-        <a href="{{ url('/') }}">Deliveboo</a>
-    </div>
-    {{-- Search --}}
-    <div id="search">
+    @guest
         <div>
-            <i class="fas fa-search"></i>
+            <a href="{{ url('/') }}">Deliveboo</a>
         </div>
-        <form action="">
-            <input type="text" placeholder="Piatti, Ristoranti o tipi di cucina">
-        </form>
-    </div>
+    {{-- Search --}}
+        <div id="search">
+            <div>
+                <i class="fas fa-search"></i>
+            </div>
+            <form action="">
+                <input type="text" placeholder="Piatti, Ristoranti o tipi di cucina">
+            </form>
+        </div>
+    @endguest
 
     {{-- Login Register --}}
-        @if (Route::has('login'))
-            <div id="links">
-                @auth
-                    <a href="{{ url('/home') }}">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
+    <div id="links">
+        @guest
+            
+            <a href="{{ route('login') }}">{{ __('Login') }}</a>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Registrati</a>
+            @endif
+            @else
+                <a id="logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
     
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Registrati</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+                
+        @endguest
+    </div>
 </header>
