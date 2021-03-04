634ab49ebe5af7a022f34774968a8785a64b48bd
Veronika <veronikakuchtova95@gmail.com>
2021-03-04 18:09:50 +0100

    css

--- deliveboo/resources/views/pages/home.blade.php
+++ deliveboo/resources/views/pages/home.blade.php
@@ -2,54 +2,53 @@
 
 @section('content')
 
-<div class="container p-2">
+<div id="dashboard" class="login-register">
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+    
+            <div>
                 <div class="card-header">
                     Dashboard di <b>{{Auth::user()->name}}</b>
                 </div>
 
-                <div class="m-3 d-flex flex-column " >
+                <div class="card-body">
                     @if (!$restaurant)
-                        <h4 class="mt-2 pb-2 border-bottom border-danger">
-                            non esiste nessun ristorante associato
-                        </h4>
-                        <a href="{{route('restaurant-create')}}" class="mt-2 w-50 btn btn-primary" >
-                            create
+                        <h3>
+                            Non esiste nessun ristorante associato
+                        </h3>
+                        <a href="{{route('restaurant-create')}}">
+                            Aggiungi ristorante
                         </a>
                     @else
 
-                        <div class="w-40 border-bottom border-danger d-flex align-items-center">
-                            <h4 class="mr-3 m-0">
+                        <div>
+                            <h3>
                                 {{$restaurant->name}}
-                            </h4>
-                            <i id='icon' class="fas fa-sort-down" onclick="show()"></i>
+                                <i id='icon' class="fas fa-sort-down" onclick="show()"></i>
+                            </h3>
                         </div>
 
-                        <div  id="box-det" class=" my-2 p-3 border border-success rounded d-none flex-column justify-content-center">
-                            <div class="m-1 d-flex align-items-center ">
-                                <h4 class="m-0 mr-2">
+                        <div  id="box-det" class="hide">
+                            <div>
+                                <h4>
                                     Via:
                                 </h4>
                                 <span>{{$restaurant->address}}</span>
                             </div>
-                            <div class="m-1 d-flex align-items-center ">
-                                <h4 class="m-0 mr-2">
+                            <div>
+                                <h4>
                                     Città:
                                 </h4>
                                 <span>{{$restaurant->city}}</span>
                             </div>
-                            <div class="m-1 d-flex align-items-center ">
+                            <div>
 
-                                <h4 class="m-0 mr-2">
+                                <h4>
                                     Piva:
                                 </h4>
                                 <span>{{$restaurant->piva}}</span>
                             </div>
-                            <div class="m-1 d-flex align-items-center">
-                                <h4 class="m-0 mr-2">
+                            <div>
+                                <h4>
                                     Phone:
                                 </h4>
                                 <span>{{$restaurant->phone}}</span>
@@ -61,19 +60,19 @@
 
                         <div class="button my-3 d-flex justify-content-around">
                             <a
-                                href="{{route('restaurant-edit', $restaurant->id)}}" class="btn btn-primary m-1" >
+                                href="{{route('restaurant-edit', $restaurant->id)}}" class="button" >
                                 Edit
                             </a>
                             <a
-                                href="{{route('dishes-index', $restaurant->id)}}" class="btn btn-primary m-1" >
+                                href="{{route('dishes-index', $restaurant->id)}}" class="button" >
                                 Menu
                             </a>
                             <a
-                                href="{{route('restaurant-destroy', $restaurant->id)}}" class="btn btn-danger m-1" >
+                                href="{{route('restaurant-destroy', $restaurant->id)}}" class="button" >
                                 Delete
                             </a>
 
-                            <a href="" class=" btn btn-primary m-1">
+                            <a href="" class="button">
                                 Stats
                             </a>
                         </div>
@@ -81,15 +80,14 @@
                     @endif
                 </div>
             </div>
-        </div>
-    </div>
+   
 </div>
 
 <script>
     function show() {
         const element = document.getElementById("box-det");
         const icon = document.getElementById('icon');
-        element.classList.toggle("d-flex");
+        element.classList.toggle("hide");
         if (icon.classList.contains('fa-sort-down')) {
             icon.classList.remove('fa-sort-down');
             icon.classList.add('fa-sort-up');
