4bff4fcb7fe8575d3ccdf8bce115a389b3a4d0fe
Veronika <veronikakuchtova95@gmail.com>
2021-03-05 23:38:48 +0100

    restaurant-dish edit + restaurant create

--- deliveboo/resources/views/pages/restaurant-edit.blade.php
+++ deliveboo/resources/views/pages/restaurant-edit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div id="restaurant-edit" class="login-register">
+    <div class="login-register edit">
         <div class="card">
             <div class="card-header">
                 Modifica le informazioni del tuo ristorate
@@ -18,7 +18,7 @@
                     <div class="container p-2 border d-flex flex-column">
                         <div class="row d-flex m-2">
                             <div class="col">
-                                <label for="name">Nome</label>
+                                <label for="name">Nome*</label>
                                 <input type="text" class="form-control" name="name" value="{{$restaurant->name}}" required>
                             </div>
                             
@@ -30,15 +30,15 @@
                         </div >
                         <div class="row d-flex m-2">
                             <div class="col">
-                                <label for="name">Indirizzo</label>
+                                <label for="name">Indirizzo*</label>
                                 <input type="text" class="form-control" name="address" value="{{$restaurant->address}}" required>
                             </div>
                             <div class="col">
-                                <label for="name">Città</label>
+                                <label for="name">Città*</label>
                                 <input type="text" class="form-control" name="city" value="{{$restaurant->city}}" required>
                             </div>
                             <div class="col">
-                                <label for="name">Numero di telefono</label>
+                                <label for="name">Numero di telefono*</label>
                                 <input type="text" class="form-control" name="phone" value="{{$restaurant->phone}}" required>
                             </div>
                         </div >
@@ -46,7 +46,7 @@
                         
                         <input type="text" hidden name="user_id" value="{{Auth::user()->id}}">
                         <div class='row ml-4'>
-                            <button type="submit" class="btn btn-primary">
+                            <button type="submit">
                                 Salva
                             </button>
                         </div>
